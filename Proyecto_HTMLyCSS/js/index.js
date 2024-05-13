fde39602bcca390cf139fc8fc814864562edf6a7
YariCB <yarimucutu@gmail.com>
2024-05-12 20:39:20 -0400

    Listado de estudiantes dinámico usando JavaScript

--- Proyecto_HTMLyCSS/js/index.js
+++ Proyecto_HTMLyCSS/js/index.js
@@ -12,22 +12,22 @@ document.addEventListener('DOMContentLoaded', function() {
           
           const source480 = document.createElement('source');
           source480.setAttribute('media', '(max-width:480px)');
-          source480.setAttribute('srcset', student.imagen);
+          source480.setAttribute('srcset', `reto5/${student.imagen}`);
           
           const sourceMin = document.createElement('source');
           sourceMin.setAttribute('media', '(min-width:481px)');
-          sourceMin.setAttribute('srcset', student.imagen);
+          sourceMin.setAttribute('srcset', `reto5/${student.imagen}`);
           
           const img = document.createElement('img');
           img.classList.add('pfpList');
-          img.setAttribute('src', student.imagen);
+          img.setAttribute('src', `reto5/${student.imagen}`);
           img.setAttribute('alt', 'Foto de ' + student.nombre);
           
           const link = document.createElement('a');
           link.setAttribute('href', 'perfil.html?ci=' + student.ci);
           link.textContent = student.nombre;
           
-          picture.appendChild(source480);
+          picture.appendChild(source480); 
           picture.appendChild(sourceMin);
           picture.appendChild(img);
           
